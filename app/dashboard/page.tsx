8060f37e001d4e662c68be46594e8142b6653df2
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-06-02 14:48:44 +0100

    feat: Create dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main className="sm:mx-auto sm:w-130 sm:py-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   const [userToken, setUserToken] = useState("");
+  const [userStatus, setUserStatus] = useState("");
 
   function logoutUser() {
     localStorage.removeItem("gistToken");
@@ -13,8 +14,12 @@ export default function Home() {
 
   useEffect(() => {
     let userToken = localStorage.getItem("gistToken");
+    let userDataJson = localStorage.getItem("gistUserData");
     userToken === "undefined" && (userToken = "");
+    userDataJson === "undefined" && (userDataJson = "");
+    const userData = userDataJson && JSON.parse(userDataJson);
     userToken && setUserToken(userToken);
+    userData?.status && setUserStatus(userData.status);
   }, []);
 
   return (
@@ -28,10 +33,18 @@ export default function Home() {
         </Link>
         {!userToken && (
           <Link
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 mb-3 px-4 sm:px-5 sm:w-auto"
             href="/login"
           >
             Login
+          </Link>
+        )}
+        {userToken && userStatus === "ADMIN" && (
+          <Link
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 mb-3 px-4 sm:px-5 sm:w-auto"
+            href="/dashboard"
+          >
+            Dashboard
           </Link>
         )}
         {userToken && (
