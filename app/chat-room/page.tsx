d5dc91fc35707d4dc682dbc5f5368fef0ca92fda
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-06-03 15:51:01 +0100

    feat: Set up a StreamChat client

--- app/chat-room/page.tsx
+++ app/chat-room/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { Chat, useCreateChatClient } from "stream-chat-react";
+
+// your Stream app information
+const apiKey = "dz5f4d5kzrue";
+const userId = "yellow-mouse-8";
+const userName = "yellow";
+const userToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoieWVsbG93LW1vdXNlLTgiLCJleHAiOjE3NDg5Nzg1ODl9.PSCR87KiCqXTOgiMcBgh3Pp2_48fZI4cLDsMinDDwBc";
+
 export default function ChatRoom() {
-  return (
-    <main className="sm:mx-auto sm:w-130 sm:py-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <h1>Chat Room</h1>
-    </main>
-  );
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: { id: userId, name: userName },
+  });
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  console.log("=== ChatRoom ===");
+  console.log(client);
+
+  return <Chat client={client}>Chat with client is ready!</Chat>;
 }
